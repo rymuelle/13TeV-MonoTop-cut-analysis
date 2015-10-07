a730d221a2682656d59b01ef567881ab1e4a63eb
Ryan Mueller <33radar@gmail.com>
2015-10-07 22:35:44 +0200

    adding section on reading branches

--- correlationStudy.C
+++ correlationStudy.C
@@ -23,7 +23,7 @@ void loadRootFiles(const std::string fileName[], int nSamples, TTree *tree[] , i
     }
 }
 
-void makeHistograms(const std::string sampleNames[], const int nSamples,  const std::string doubleVariableNames[],const  int nDoubleVariables, TH1F **TH1F_histograms){
+void makeHistograms(const std::string sampleNames[], const int nSamples,  const std::string doubleVariableNames[],const int nDoubleVariables, const int nBins, const int minValue[], const int maxValue[], TH1F **TH1F_histograms){
 
     for (int i = 0; i < nSamples; i++){
         TH1F_histograms[i] = new TH1F [nDoubleVariables];
@@ -36,12 +36,29 @@ void makeHistograms(const std::string sampleNames[], const int nSamples,  const 
             std::cout << histName << std::endl;
             TH1F_histograms[i][j].SetName(histName.c_str());
             TH1F_histograms[i][j].SetTitle(histName.c_str());
-            TH1F_histograms[i][j].SetBinsLenght(
+            TH1F_histograms[i][j].SetBinsLength(nBins);
+            TH1F_histograms[i][j].SetMinimum(minValue[j]);
+            TH1F_histograms[i][j].SetMaximum(maxValue[j]);
         }
     } 
 
     
 }
+
+void getBranches(const std::string doubleVariableNames[], const int nDoubleVariables, const int doubleArraySize[], double  ***doubleVariables, TTree *sampleTrees[], const int nSamples){
+
+    for(int i = 0; i < nSamples; i++){
+        doubleVariables[i] = new double*[nDoubleVariables];
+        for(int j = 0; j < nDoubleVariables; j++){
+            doubleVariables[i][j] = new double [doubleArraySize[j]];
+        }
+        
+    }
+
+
+
+}
+
 
 void plotHistograms(){
 
@@ -60,6 +77,7 @@ void correlationStudy(){
     const std::string doubleVariableNames[] =   {"MetPt",   "PFMuonPt"};
     const int minValue[] =                      {10,        10};
     const int maxValue[] =                      {700,       700};
+    const int doubleArraySize[] =               {40,        13};
 
     const int nBins = 100;
     const int nDoubleVariables = sizeof(doubleVariableNames)/sizeof(doubleVariableNames[0]);
@@ -67,7 +85,7 @@ void correlationStudy(){
 
     ///variables
     int sampleLength[nSamples];
-    double doubleVariables[nDoubleVariables];
+    double ***doubleVariables = new double**[nSamples];
 
     //root objects
     TTree *sampleTrees[nSamples];
@@ -77,7 +95,9 @@ void correlationStudy(){
 
     loadRootFiles(sampleNames, nSamples, sampleTrees, sampleLength);
 
-    makeHistograms(sampleNames, nSamples, doubleVariableNames, nDoubleVariables, TH1F_histograms);
+    makeHistograms(sampleNames, nSamples, doubleVariableNames, nDoubleVariables, nBins, minValue, maxValue,  TH1F_histograms);
+
+    getBranches(doubleVariableNames, nDoubleVariables, doubleArraySize, doubleVariables, sampleTrees, nSamples);    
 
     plotHistograms();
 
