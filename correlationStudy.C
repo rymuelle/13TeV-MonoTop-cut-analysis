99f82bfa102c4f1a0c7d0e79147f9ea814771107
Ryan Mueller <33radar@gmail.com>
2015-10-07 22:05:53 +0200

    updates to making histograms

--- correlationStudy.C
+++ correlationStudy.C
@@ -12,9 +12,9 @@ void loadRootFiles(const std::string fileName[], int nSamples, TTree *tree[] , i
     std::string dirName;
 
     for(int i = 0; i < nSamples; i++){
-
-        f = TFile::Open(fileName[i].c_str());
-        dirName = fileName[i] + ":/NtupleAnalyzer";
+        dirName = fileName[i] + ".root";
+        f = TFile::Open(dirName.c_str());
+        dirName = dirName + ":/NtupleAnalyzer";
         dir = (TDirectory*)f->Get(dirName.c_str());
         dir->ls();
         tree[i] =  (TTree*)dir->Get("ntuple");
@@ -23,8 +23,24 @@ void loadRootFiles(const std::string fileName[], int nSamples, TTree *tree[] , i
     }
 }
 
-void makeHistograms(){
+void makeHistograms(const std::string sampleNames[], const int nSamples,  const std::string doubleVariableNames[],const  int nDoubleVariables, TH1F **TH1F_histograms){
 
+    for (int i = 0; i < nSamples; i++){
+        TH1F_histograms[i] = new TH1F [nDoubleVariables];
+    }
+
+    std::string histName;
+    for(int i = 0; i < nSamples; i++){
+        for(int j = 0; j < nDoubleVariables; j++){
+            histName = "TH1F_" + sampleNames[i] + "_" + doubleVariableNames[j];
+            std::cout << histName << std::endl;
+            TH1F_histograms[i][j].SetName(histName.c_str());
+            TH1F_histograms[i][j].SetTitle(histName.c_str());
+            TH1F_histograms[i][j].SetBinsLenght(
+        }
+    } 
+
+    
 }
 
 void plotHistograms(){
@@ -38,17 +54,30 @@ void drawHistograms(){
 void correlationStudy(){
     
     ///constants
-    const std::string sampleNames[] = {"MonoTop.root", "WJet.root", "TTbar.root"};
+    const std::string sampleNames[] = {"MonoTop", "WJet", "TTbar"};
     const int nSamples = sizeof(sampleNames)/sizeof(sampleNames[0]);
+
+    const std::string doubleVariableNames[] =   {"MetPt",   "PFMuonPt"};
+    const int minValue[] =                      {10,        10};
+    const int maxValue[] =                      {700,       700};
+
+    const int nBins = 100;
+    const int nDoubleVariables = sizeof(doubleVariableNames)/sizeof(doubleVariableNames[0]);
+    
 
     ///variables
     int sampleLength[nSamples];
-   
+    double doubleVariables[nDoubleVariables];
+
+    //root objects
     TTree *sampleTrees[nSamples];
+
+    TH1F **TH1F_histograms = new TH1F*[nSamples];
+
 
     loadRootFiles(sampleNames, nSamples, sampleTrees, sampleLength);
 
-    makeHistograms();
+    makeHistograms(sampleNames, nSamples, doubleVariableNames, nDoubleVariables, TH1F_histograms);
 
     plotHistograms();
 
